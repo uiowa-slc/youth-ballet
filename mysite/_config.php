30c6fc6eefb23ce9fa35e50dce9187c936a24259
Dustin Quam <quamsta@gmail.com>
2014-03-27 12:19:17 -0500

    added config

---
+++ mysite/_config.php
@@ -0,0 +1,20 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'youthballet';
+
+require_once("conf/ConfigureFromEnv.php");
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('simple');
+
+// Set the site locale
+i18n::set_locale('en_US');
+FulltextSearchable::enable();
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
