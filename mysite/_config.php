a3403f15003f334aa7e565e47a4b9dbc43f20e81
Dustin Quam <quamsta@gmail.com>
2017-07-26 12:41:34 -0500

    updated modules, config and added AD

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,9 +8,6 @@ $database = 'youthballet';
 
 require_once("conf/ConfigureFromEnv.php");
 
-Calendar::add_extension('CalendarExtension');
-CalendarEvent::add_extension('CalendarEventExtension');
-
 MySQLDatabase::set_connection_charset('utf8');
 
 // Set the current theme. More themes can be downloaded from
@@ -22,12 +19,9 @@ FulltextSearchable::enable(array('SiteTree'));
 // Set the site locale
 i18n::set_locale('en_US');
 
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-
-SiteConfig::add_extension('SiteConfigExtension');
 
 if(Director::isLive()) {
 	Director::forceSSL();
 }
 GD::set_default_quality(80);
+Authenticator::set_default_authenticator('SAMLAuthenticator');
